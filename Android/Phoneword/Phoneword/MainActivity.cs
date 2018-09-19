ab2eff8a7c24247f7b463c91e48998f3c72b8679
yb123speed <904896209@qq.com>
2018-09-19 16:48:41 +0800

    add android phoneword demo

---
+++ Android/Phoneword/Phoneword/MainActivity.cs
@@ -0,0 +1,56 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+using Android.Content;
+
+namespace Phoneword
+{
+    [Activity(Label = "Phone Word", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        static readonly List<string> phoneNumbers = new List<string>();
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            TextView translatedPhoneword = FindViewById<TextView>(Resource.Id.TranslatedPhoneWord);
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+
+            Button translationHistoryButton = FindViewById<Button>(Resource.Id.TranslationHistoryButton);
+
+            string translatedNumber = string.Empty;
+
+            translateButton.Click += (sender, e) =>
+             {
+                translatedNumber = PhoneTranslator.ToNumber(phoneNumberText.Text);
+                if(!string.IsNullOrWhiteSpace(translatedNumber))
+                {
+                     translatedPhoneword.Text = translatedNumber;
+                     phoneNumbers.Add(translatedNumber);
+                     translationHistoryButton.Enabled = true;
+                     
+                }
+                else
+                {
+                     translatedPhoneword.Text = string.Empty;
+                }
+             };
+
+            translationHistoryButton.Click += (sender, e)=>
+            {
+                var intent = new Intent(this, typeof(TranslationHistoryActivity));
+                intent.PutStringArrayListExtra("phone_numbers", phoneNumbers);
+                StartActivity(intent);
+
+            };
+
+        }
+    }
+}
+
---
+++ Android/Phoneword/Phoneword/TranslationHistoryActivity.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Phoneword
+{
+    [Activity(Label = "@string/translationHistory")]
+    public class TranslationHistoryActivity : ListActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+
+            var phoneNumbers = Intent.Extras.GetStringArrayList("phone_numbers" ) ?? new string[0];
+            this.ListAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, phoneNumbers);
+        }
+    }
+}
